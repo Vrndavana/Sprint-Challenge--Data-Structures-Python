8401dcdc0714de68ca8485e571bbe8b0975f9218
Vrndavana <Vrmdavama.th@gmail.com>
2020-06-12 08:55:28 -0700

    Names

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -2,52 +2,35 @@ class Node:
     def __init__(self, value=None, next_node=None):
         self.value = value
         self.next_node = next_node
-
     def get_value(self):
         return self.value
-
     def get_next(self):
         return self.next_node
-
     def set_next(self, new_next):
         self.next_node = new_next
-
 class LinkedList:
     def __init__(self):
         self.head = None
-
     def add_to_head(self, value):
         node = Node(value)
-
         if self.head is not None:
             node.set_next(self.head)
-
         self.head = node
-
     def contains(self, value):
         if not self.head:
             return False
-
         current = self.head
-
         while current:
             if current.get_value() == value:
                 return True
-
             current = current.get_next()
-
         return False
 
     def reverse_list(self, node, prev):
-     if not self.head:
-            return
-        if not node.next_node:
-            self.head = node
-            node.next_node = prev
-            return
-
-        next = node.next_node
-
-        node.next_node = prev
-
-        self.reverse_list(next, node)
+        current_node = node
+        while current_node is not None:
+            next = current_node.next_node
+            current_node.next_node = prev
+            prev = current_node
+            current_node = next
+        self.head = prev
